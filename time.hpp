101440bbe61c7d2909481442c7ac64ed301d9ef9
Aret N Carlsen <AretCarlsen@autonomoustools.com>
2010-05-17 16:39:40 -0500

    Small updates.

--- time.hpp
+++ time.hpp
@@ -1,16 +1,27 @@
 #pragma once
 
-#include "../DataStore/AtomicBuffer.hpp"
-
-void sleep_us(uint32_t delay){
-  for(uint32_t endTime = get_systemTime() + delay; get_systemTime() < endTime; );
+inline void sleep_us(SystemTime_t delay){
+/*
+  SystemTime_t lastTime = get_systemTime();
+  DEBUGprint("Dly X%x; init T X%x;", delay, lastTime);
+*/
+  for(SystemTime_t endTime = get_systemTime() + delay; get_systemTime() < endTime; );
+/* {
+    SystemTime_t tmpTime = get_systemTime();
+    if(tmpTime > lastTime + 1000){
+      DEBUGprint_FORCE("%d;", tmpTime);
+      lastTime = tmpTime;
+    }
+  }
+  DEBUGprint("Dly cmplt;");
+*/
 }
 inline void sleep_ms(uint32_t delay){
   sleep_us(1000 * delay);
 }
 
-// Snap a process period to the nearest SystemTimer value.
-SystemTimer_t snapTo(SystemTimer_t value, SystemTimer_t period){
+// Snap a process period to the nearest SystemTime value.
+SystemTime_t snapTo(SystemTime_t value, const SystemTime_t period){
   if(value % period != 0)
     value += period - (value % period);
 
